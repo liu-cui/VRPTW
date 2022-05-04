ba988d1968a781d442af849ce5adc8b3d9877e39
liu-cui <15301638120@163.com>
2022-05-05 06:12:19 +0800

    1.add app.py

--- src/test.py
+++ src/test.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     # print(data.cor_x)
     # print(data.cor_y)
     active_arcs = gb_solver(data, vehicle_num=5)
-    for i, j in active_arcs:
-        plt.plot([data.cor_x[i], data.cor_x[j]], [data.cor_x[i], data.cor_y[j]], c='g', zorder=0)
-    plt.plot(data.cor_x[0], data.cor_y[0], c='r', marker='s')
-    plt.scatter(data.cor_x[1:-1], data.cor_y[1:-1], c='b')
+    # for i, j in active_arcs:
+    #     plt.plot([data.cor_x[i], data.cor_x[j]], [data.cor_x[i], data.cor_y[j]], c='g', zorder=0)
+    # plt.plot(data.cor_x[0], data.cor_y[0], c='r', marker='s')
+    # plt.scatter(data.cor_x[1:-1], data.cor_y[1:-1], c='b')
     plt.show()
